f197a97d161d68dbecc9232fdcd30e6c8cc2888b
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-06-01 02:56:10 +0530

    first commit

--- project-management/src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -1,14 +1,30 @@
 package com.jrp.pma.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
-	
-	@RequestMapping("/new")
-	public String displayController() {
+
+	@GetMapping("/new")
+	public String displayProjectForm(Model model) {
+		Project aProject = new Project();
+
+		model.addAttribute("project", aProject);
+		return "new-project";
+	}
+
+	@PostMapping("/save")
+	public String createProject(Project project, Model model) {
+		Project aProject = new Project();
+
+		model.addAttribute("project", aProject);
 		return "new-project";
 	}
 
