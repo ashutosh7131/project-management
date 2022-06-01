05c0836d895daa53ef5da49e29321f08dac9487d
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-06-01 13:58:19 +0530

    final commit

--- project-management/src/main/java/com/jrp/pma/entities/Project.java
+++ project-management/src/main/java/com/jrp/pma/entities/Project.java
@@ -1,21 +1,60 @@
 package com.jrp.pma.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Project {
-
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long projectId;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="project_seq")
+	private long projectId;
+	
 	private String name;
-	private String stage;
+	
+	private String stage; // NOTSTARTED, COMPLETED, INPROGRESS
+	
 	private String description;
-
+	
+	@NotBlank(message="date cannot be empty")
+	private Date startDate;
+	
+	@NotBlank(message="date cannot be empty")
+	private Date endDate;
+	
+	@ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST},
+			   fetch = FetchType.LAZY)
+	@JoinTable(name="project_employee",
+				joinColumns=@JoinColumn(name="project_id"), 
+				inverseJoinColumns= @JoinColumn(name="employee_id")
+	)
+	@JsonIgnore
+	private List<Employee> employees;
+	
 	public Project() {
+		
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
 	}
 
 	public Project(String name, String stage, String description) {
@@ -25,11 +64,11 @@ public class Project {
 		this.description = description;
 	}
 
-	public Long getProjectId() {
+	public long getProjectId() {
 		return projectId;
 	}
 
-	public void setProjectId(Long projectId) {
+	public void setProjectId(long projectId) {
 		this.projectId = projectId;
 	}
 
@@ -56,5 +95,29 @@ public class Project {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	// convenience method:
+	public void addEmployee(Employee emp) {
+		if(employees==null) {
+			employees = new ArrayList<>();
+		}
+		employees.add(emp);
+	}
 
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
 }
